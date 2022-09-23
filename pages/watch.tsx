a2d789e0077fe965f1c2e90d7068de83b87a33d6
madbull12 <huangandrian02@gmail.com>
2022-09-24 00:07:26 +0700

    working on watch page

--- pages/watch.tsx
+++ pages/watch.tsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import { useRouter } from "next/router";
 import React from "react";
 import Body from "../components/Body";
@@ -10,16 +11,30 @@ const VideoPage = () => {
   const { data, loading, error } = useFetch(
     `videos?part=contentDetails,snippet,statistics&id=${v}`
   );
+
+//   const { data:channelData } = useFetch(`channels?part=snippet,statistics&id=${data?.items[0]?.snippet?.channelId}`)
+
   console.log(data);
+//   console.log(channelData)
   return (
     <Body>
       <div className="flex gap-x-2">
-        <div className="w-2/3">
+        <div className="w-[60%] space-y-2">
           <iframe
-            width="420"
-            height="345"
-            src="https://www.youtube.com/embed/tgbNymZ7vqY"
+            height={400}
+            className="w-full "
+            src={`https://www.youtube.com/embed/${v}`}
           ></iframe>
+          <h1 className="text-xl text-white">{data?.items[0]?.snippet?.title}</h1>
+          <div className="gap-x-3 flex items-center">
+            <p className="text-gray-400 ">
+                {Number(data?.items[0]?.statistics?.viewCount).toLocaleString()} views
+            </p>
+            <p className="text-gray-400 text-sm">
+                {moment(data?.items[0]?.snippet?.publishedAt).format("ll")}
+            </p>
+          </div>
+       
         </div>
       </div>
     </Body>
