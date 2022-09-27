9702aefdf0ab514c56481531fe32d73b04bff16a
madbull12 <huangandrian02@gmail.com>
2022-09-27 11:12:30 +0700

    working on search filter

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,25 +1,31 @@
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Body from "../components/Body";
 import SearchSnippet from "../components/SearchSnippet";
 import useSearch from "../hooks/useSearch";
 import useFetch from "../hooks/useSearch";
 import { IChannel, IVideo, IVideoDetails, IVideoInfo } from "../interface";
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 import ChannelSnippet from "../components/ChannelSnippet";
+import { AiFillFilter } from "react-icons/ai";
 
-
-type Props= {
-  item:IVideo | IChannel
-}
+type Props = {
+  item: IVideo | IChannel;
+};
 
 const SearchPage = () => {
   const router = useRouter();
   const { results } = router.query;
-  const [didYouMean,setDidYouMean] = useState<string | null>(null);
+  const [cursorToken, setCursorToken] = useState<string>("");
+  const [didYouMean, setDidYouMean] = useState<string | null>(null);
+  useEffect(()=>{
+    setCursorToken("")
+  },[router.pathname]);
 
   // const {data,loading,error} = useFetch(`search?q=${results}&part=snippet,id&maxResults=50`);
-  const { data, loading, error } = useSearch(`?q=${didYouMean ? didYouMean : results}`);
+  const { data, loading, error } = useSearch(
+    `?q=${didYouMean ? didYouMean : results}&cursor=${cursorToken}`
+  );
 
   console.log(data);
 
@@ -29,39 +35,67 @@ const SearchPage = () => {
 
   return (
     <Body>
-      <div className="flex flex-col space-y-4">
+      <div className="flex flex-col space-y-4  ">
         {/* {data?.contents.map((item:IVideo)=>(
                 <SearchSnippet video={item} />
             ))} */}
+        <button className="font-semibold text-gray-400 flex items-center">
+          <AiFillFilter />
+          <span>FILTERS</span>
+        </button>
+        <div className="w-full height-0">
+          <div className="flex gap-x-8 justify-between  ">
+            {data?.filterGroups.map((item: any) => (
+              <div>
+                <p className="text-white font-semibold uppercase text-sm border-b border-gray-400">
+                  {item.title}
+                </p>
+                <div className="pt-4 space-y-3 cursor-pointer">
+                  {item.filters.map((filter: any) => (
+                    <p
+                      onClick={()=>setCursorToken(filter.cursorSelect)}
+                      className={`${
+                        filter.selected ? "text-white" : "text-gray-400"
+                      }  text-sm`}
+                    >
+                      {filter.label}
+                    </p>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
         <>
           {data?.didYouMean && (
             <p className="text-white">
-              Did you mean <span onClick={()=>setDidYouMean(data?.didYouMean)} className="font-semibold cursor-pointer">
-              {data?.didYouMean}?
+              Did you mean{" "}
+              <span
+                onClick={() => setDidYouMean(data?.didYouMean)}
+                className="font-semibold cursor-pointer"
+              >
+                {data?.didYouMean}?
               </span>
-             
             </p>
           )}
         </>
-        {data?.contents
-          .filter((item:IChannel)=>item.type==="channel")
-          .map((item:IChannel)=>(
-            <div className="my-8" key={uuidv4()}>
-              <ChannelSnippet channel={item} />
+        <div>
+          {data?.contents
+            .filter((item: IChannel) => item.type === "channel")
+            .map((item: IChannel) => (
+              <div className="py-6" key={uuidv4()}>
+                <ChannelSnippet channel={item} />
+              </div>
+            ))}
+        </div>
 
-
-            </div>
-        ))}
-
-        {data?.contents
-          .filter((item:IVideo)=>item.type==="video")
-          .map((item:IVideo)=>(
-            <SearchSnippet video={item} key={uuidv4()} />
-          ))}
-             
-
-       
-                
+        <div className="py-8 space-y-4">
+          {data?.contents
+            .filter((item: IVideo) => item.type === "video")
+            .map((item: IVideo) => (
+              <SearchSnippet video={item} key={uuidv4()} />
+            ))}
+        </div>
       </div>
     </Body>
   );
