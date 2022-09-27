15971a254ee25746f269eac2c0a3a07273a64cf9
madbull12 <huangandrian02@gmail.com>
2022-09-27 15:55:54 +0700

    working on video snippet

--- components/VideoSnippet.tsx
+++ components/VideoSnippet.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import ReactTimeAgo from "react-time-ago";
 import nFormatter from "../helper/convertion";
 import { ISnippet, IVideo, IVideoDetails, IVideoInfo } from "../interface";
+import Avatar from "./Avatar";
 
 interface IProps {
   video: IVideo;
@@ -12,21 +13,29 @@ interface IProps {
 const VideoSnippet = ({ video,column }: IProps) => {
   return (
     <Link href={`/watch?v=${video.video.videoId}`}>
-      <div className={`${column ? "flex-col": "flex-row"}  gap-2 cursor-pointer`}>
+      <div className={`${column ? "flex-col": "flex-row"} flex  gap-2 cursor-pointer`}>
         <Image
           src={video?.video.thumbnails[0].url}
           height={video?.video.thumbnails[0].height}
           objectFit="cover"
           width={video?.video.thumbnails[0].width}
+        
           className="w-1/2"
         />
-        <div className="w-1/2">
-          <h1 className="text-md text-white">{video.video.title}</h1>
-          <Link href="/">
-            <p className="text-gray-400 text-sm">
-              {video.video.author?.title}
-            </p>
-          </Link>
+        <div className={`${column ? "w-full" : " w-1/2"} space-y-2`}>
+          <h1 className="text-md  text-white">{video.video.title}</h1>
+          {video.video.author && (
+              <Link href="/">
+                <div className="flex items-center gap-x-2">
+                  <Avatar src={video.video.author?.avatar[0].url ?? ""} width={30} height={30} />
+                  <p className="text-gray-400 text-sm">
+                    {video.video.author?.title}
+                  </p>
+                </div>
+            
+            </Link>
+          )}
+     
           <div className="flex items-center gap-x-2">
             <p className="text-gray-400 text-sm">
               {nFormatter(video.video.stats.views)} views
