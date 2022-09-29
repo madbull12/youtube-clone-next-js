98d62315e639778c6585aa3e5d5432ba14c5b5b5
madbull12 <huangandrian02@gmail.com>
2022-09-29 13:12:27 +0700

    use debounce hook

--- components/VideoSnippet.tsx
+++ components/VideoSnippet.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React from "react";
 import ReactTimeAgo from "react-time-ago";
 import nFormatter from "../helper/convertion";
+import toHHMS from "../helper/toHHMS";
 import { ISnippet, IVideo, IVideoDetails, IVideoInfo } from "../interface";
 import Avatar from "./Avatar";
 
@@ -15,14 +16,20 @@ const VideoSnippet = ({ video,column }: IProps) => {
   return (
     <Link href={`/watch?v=${video.video.videoId}`}>
       <div className={`${column ? "flex-col": "flex-row"} flex  gap-2 cursor-pointer`}>
-        <Image
-          src={video?.video.thumbnails[3]?.url ?? video?.video.thumbnails[0].url}
-          height={video?.video.thumbnails[3]?.height ?? video?.video.thumbnails[0].height}
-          objectFit="cover"
-          width={video?.video.thumbnails[3]?.width ?? video?.video.thumbnails[0].width}
-        
-          className="w-1/2"
-        />
+        <div className="relative ">
+          <Image
+            src={video?.video.thumbnails[3]?.url ?? video?.video.thumbnails[0].url}
+            height={video?.video.thumbnails[3]?.height ?? video?.video.thumbnails[0].height}
+            objectFit="cover"
+            width={video?.video.thumbnails[3]?.width ?? video?.video.thumbnails[0].width}
+          
+            className="w-1/2"
+          />
+          <div className="bg-black opacity-75 text-white text-xs p-1 right-2 rounded-sm absolute bottom-2">
+            {toHHMS(video.video.lengthSeconds?.toString())}
+          </div>
+        </div>
+     
         <div className={`${column ? "w-full" : " w-1/2"} space-y-2`}>
           <h1 className="text-md  text-white">{video.video.title}</h1>
           {video.video.author && (
