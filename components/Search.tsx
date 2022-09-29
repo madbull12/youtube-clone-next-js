67ea915918264c37a577c3943e8c05b0dbeb1f13
madbull12 <huangandrian02@gmail.com>
2022-09-30 00:34:57 +0700

    working on autocomplete

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,7 +1,8 @@
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useRef } from "react";
 import { GoSearch } from "react-icons/go";
 import useDebounce from "../hooks/useDebounce";
+import useOutsideClick from "../hooks/useOutsideClick";
 import { youtubeAutoComplete } from "../lib/axios";
 
 const Search = () => {
@@ -9,9 +10,15 @@ const Search = () => {
   const [data,setData] = useState<any>(null);
   const [term, setTerm] = useState<string>("");
   const router = useRouter();
-
+  // const [openAutocomplete,setOpenAutocomplete] = useState<boolean>(true);
 
   const debouncedSearch = useDebounce(term,500);
+
+  const ref = useRef(null);
+
+  useOutsideClick(ref, () => {
+    setTerm("")
+  });
 
   useEffect(()=>{
     // https://youtube138.p.rapidapi.com/auto-complete/
@@ -40,6 +47,8 @@ const Search = () => {
           e.preventDefault();
           router.push(`/search?results=${term}`);
         }}
+        
+
         onFocus={() => setFocus(true)}
         onBlur={() => setFocus(false)}
         className={`flex relative  border px-2 gap-x-2 items-center  ${
@@ -49,7 +58,7 @@ const Search = () => {
         {focus && <GoSearch className="text-white" />}
         <input
           onChange={(e) => setTerm(e.target.value)}
-          type="text"
+          type="search"
           placeholder="Search"
           value={term}
           className="text-white  w-full outline-none bg-transparent  px-1 py-2"
@@ -61,8 +70,8 @@ const Search = () => {
           <GoSearch className="text-white" />
         </button>
       </form>
-      {(debouncedSearch && data) && (
-          <div className="absolute  w-full py-2  top-full bg-white">
+      {(debouncedSearch && data ) && (
+          <div className="absolute  w-full py-2  top-full bg-white" ref={ref}>
                 {data?.results.map((item:string)=>(
                   <div onClick={()=>setTerm(item)} className="flex gap-x-2 items-center py-1 px-2 hover:bg-gray-100 cursor-pointer">
                     <GoSearch />
