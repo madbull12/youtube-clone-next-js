0317c2cde2aabcc73041df6dcea204796519ef4c
madbull12 <huangandrian02@gmail.com>
2022-11-27 18:56:00 +0700

    ui changes

--- components/ChannelSnippet.tsx
+++ components/ChannelSnippet.tsx
@@ -11,8 +11,8 @@ const ChannelSnippet = ({ channel }: IProps) => {
   console.log(channel);
   return (
     <Link href={`/channel/${channel.channel.channelId}/feature`}>
-      <div className=" flex items-center mx-auto max-w-4xl  gap-x-16 cursor-pointer">
-        <div className="w-32 h-32 relative">
+      <div className=" flex items-center flex-col lg:flex-row gap-y-4 justify-center mx-auto max-w-4xl  gap-x-16 cursor-pointer">
+        <div className="md:w-32 md:h-32 w-24 h-24 relative">
           <Image
             src={channel.channel.avatar[1].url ?? ""}
             layout="fill"
@@ -21,7 +21,7 @@ const ChannelSnippet = ({ channel }: IProps) => {
           />
         </div>
   
-        <div className="max-w-sm">
+        <div className="max-w-sm lg:text-start text-center text-sm md:text-base">
           <p className="font-semibold text-white">{channel.channel.title}</p>
           <p className="text-sm text-gray-400">
             {channel.channel.stats.subscribersText}
@@ -30,7 +30,7 @@ const ChannelSnippet = ({ channel }: IProps) => {
             {channel.channel.descriptionSnippet}
           </p>
         </div>
-        <button className="bg-[#CC0000] rounded-sm text-white font-semibold px-4 ml-auto py-2">
+        <button className="bg-[#CC0000] rounded-sm text-white font-semibold px-4 lg:ml-auto py-2">
           Subscribe
         </button>
       </div>
