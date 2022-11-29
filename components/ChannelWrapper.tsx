32341e6f9cd780839080d5f48beb48448e539575
madbull12 <huangandrian02@gmail.com>
2022-11-29 13:30:44 +0700

    test build

--- components/ChannelWrapper.tsx
+++ components/ChannelWrapper.tsx
@@ -6,7 +6,7 @@ import useChannelDetails from "../hooks/useChannelDetails";
 import { IChannelDetails } from "../interface";
 import Avatar from "./Avatar";
 import Body from "./Body";
-import NoImage from '../public/no-image.png'
+import NoImage from "../public/no-image.png";
 import useChannelChannels from "../hooks/useChannelChannels";
 
 interface IProps {
@@ -19,100 +19,123 @@ const ChannelWrapper = ({ children }: IProps) => {
   // useEffect(()=>{
   //     router.replace("/")
   // },[]);
-  const { data:channel, loading,error } = useChannelDetails(`?id=${channelId}`);
+  const {
+    data: channel,
+    loading,
+    error,
+  } = useChannelDetails(`?id=${channelId}`);
   console.log(channel);
 
-  if(loading) return <p>Loading...</p>
+  if (loading) return <p>Loading...</p>;
 
   return (
     <>
-       {channel !== null && (
-      <div>
-      {channel.banner.desktop !== null ? (
-      <Image
-        src={channel?.banner.desktop[0].url}
-        height={channel?.banner.desktop[2].height}
-        width={channel?.banner.desktop[2].width}
-      />
-      ):(
-        <Image
-          src={NoImage}
-          height={150}
-          width={200}
-      />
-      )}
-  
-      <div className="flex items-center justify-between mt-4 px-8">
-        <div className="flex items-center gap-x-4">
-          <Avatar
-            src={channel?.avatar[1].url}
-            height={channel?.avatar[1].height}
-            width={channel?.avatar[1].width}
-          />
-          <div>
-            <p className="text-white text-lg">{channel?.title}</p>
-            <p className="text-gray-400">{channel?.stats.subscribersText}</p>
+      {channel !== null && (
+        <div>
+          {channel.banner.desktop !== null ? (
+            <Image
+              src={channel?.banner.desktop[0].url}
+              height={channel?.banner.desktop[2].height}
+              width={channel?.banner.desktop[2].width}
+            />
+          ) : (
+            <Image src={NoImage} height={150} width={200} />
+          )}
+
+          <div className="flex items-center justify-between mt-4 px-8">
+            <div className="flex items-center gap-x-4">
+              <Avatar
+                src={channel?.avatar[1].url}
+                height={channel?.avatar[1].height}
+                width={channel?.avatar[1].width}
+              />
+              <div>
+                <p className="text-white text-lg">{channel?.title}</p>
+                <p className="text-gray-400">
+                  {channel?.stats.subscribersText}
+                </p>
+              </div>
+            </div>
+            <button className="bg-[#c91515] text-white px-4 py-2 rounded-sm font-semibold text-sm">
+              SUBSCRIBE
+            </button>
           </div>
+          <ul className="text-white font-semibold mt-4 flex items-center w-1/2 justify-between gap-x-4">
+            <li
+              className={` text-gray-400 ${
+                router.pathname.includes("/feature")
+                  ? "font-semibold text-white border-b-2"
+                  : null
+              }`}
+            >
+              <Link
+                href={`/channel/${channelId}/feature`}
+                as={`/channel/${channelId}/feature`}
+              >
+                HOME
+              </Link>
+            </li>
+            <li
+              className={` text-gray-400 ${
+                router.pathname.includes("/videos")
+                  ? "font-semibold text-white border-b-2"
+                  : null
+              }`}
+            >
+              <Link
+                href={`/channel/${channelId}/videos`}
+                as={`/channel/${channelId}/videos`}
+              >
+                VIDEOS
+              </Link>
+            </li>
+            <li
+              className={` text-gray-400 ${
+                router.pathname.includes("/playlists")
+                  ? "font-semibold text-white border-b-2"
+                  : null
+              }`}
+            >
+              <Link
+                href={`/channel/${channelId}/playlists`}
+                as={`/channel/${channelId}/playlists`}
+              >
+                PLAYLISTS
+              </Link>
+            </li>
+            <li
+              className={` text-gray-400 ${
+                router.pathname.includes("/channels")
+                  ? "font-semibold text-white border-b-2"
+                  : null
+              }`}
+            >
+              <Link
+                href={`/channel/${channelId}/channels`}
+                as={`/channel/${channelId}/channels`}
+              >
+                CHANNELS
+              </Link>
+            </li>
+            <li
+              className={` text-gray-400 ${
+                router.pathname.includes("/about")
+                  ? "font-semibold text-white border-b-2"
+                  : null
+              }`}
+            >
+              <Link
+                href={`/channel/${channelId}/about`}
+                as={`/channel/${channelId}/about`}
+              >
+                ABOUT
+              </Link>
+            </li>
+          </ul>
+          {children}
         </div>
-        <button className="bg-[#c91515] text-white px-4 py-2 rounded-sm font-semibold text-sm">
-          SUBSCRIBE
-        </button>
-      </div>
-      <ul className="text-white font-semibold mt-4 flex items-center w-1/2 justify-between gap-x-4">
-        <li
-          className={` text-gray-400 ${
-            router.pathname.includes("/feature")
-              ? "font-semibold text-white border-b-2"
-              : null
-          }`}
-        >
-          <Link href={`/channel/${channelId}/feature`}  as={`/channel/${channelId}/feature`}>
-            HOME
-          </Link>
-        </li>
-        <li
-          className={` text-gray-400 ${
-            router.pathname.includes("/videos")
-              ? "font-semibold text-white border-b-2"
-              : null
-          }`}
-        >
-          <Link href={`/channel/${channelId}/videos`} as={`/channel/${channelId}/videos`}>VIDEOS</Link>
-        </li>
-        <li
-          className={` text-gray-400 ${
-            router.pathname.includes("/playlists")
-              ? "font-semibold text-white border-b-2"
-              : null
-          }`}
-        >
-          <Link href={`/channel/${channelId}/playlists`} as={`/channel/${channelId}/playlists`}>PLAYLISTS</Link>
-        </li>
-        <li
-          className={` text-gray-400 ${
-            router.pathname.includes("/channels")
-              ? "font-semibold text-white border-b-2"
-              : null
-          }`}
-        >
-          <Link href={`/channel/${channelId}/channels`} as={`/channel/${channelId}/channels`}>CHANNELS</Link>
-        </li>
-        <li
-          className={` text-gray-400 ${
-            router.pathname.includes("/about")
-              ? "font-semibold text-white border-b-2"
-              : null
-          }`}
-        >
-          <Link href={`/channel/${channelId}/about`} as={`/channel/${channelId}/about`}>ABOUT</Link>
-        </li>
-      </ul>
-      {children}
-    </div>
       )}
     </>
- 
-    
   );
 };
 
