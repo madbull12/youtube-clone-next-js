bef6a7a3763cc0c8d0c1656e3eaa2a1299fbb63b
madbull12 <huangandrian02@gmail.com>
2022-11-30 11:00:04 +0700

    working on  channel's playlist page

---
+++ hooks/useChannelPlaylists.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+import { useQuery } from '@tanstack/react-query'
+import  { youtubeChannelDetails, youtubeChannelPlaylists } from "../lib/axios"
+
+export default function useChannelPlaylists(url:string){
+
+    const fetchChannelPlaylists = async()=>{
+        const res = await youtubeChannelPlaylists(url);
+        return res.data;
+    }
+
+    const { data,isLoading:loading,error,refetch } =  useQuery({
+        queryKey: ["channelDetailsQuery"],
+        queryFn:fetchChannelPlaylists,
+        
+    });
+
+    useEffect(()=>{
+        refetch()
+    },[url])
+
+    return { data, error, loading }
+
+}
